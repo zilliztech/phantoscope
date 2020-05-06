efd3170f55dcf771fb14ca23da5b0f56edd2bd65
れいげん あらたか <boyan.wang@zilliz.com>
2020-05-06 11:50:12 +0800

    delete useless code

--- search/common/error.py
+++ search/common/error.py
@@ -1,107 +1,66 @@
 class Error(Exception):
+    def __init__(self, message, error):
+        self.message = message
+        self.error = error
+
     @property
     def error_code(self):
         return 503
 
 
 class OperatorImportError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
-
+    pass
 
 class PipelineCheckError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
-
+    pass
 
 class Insert2SQLError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
-
+    pass
 
 class QueryFromSQLError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
-
+    pass
 
 class DeleteFromSQLError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
-
+    pass
 
 class UpdateFromSQLError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
-
+    pass
 
 class NotExistError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
-
+    pass
 
 class MilvusError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
-
+    pass
 
 class S3Error(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
+    pass
 
 
 class DecodeError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
-
     @property
     def error_code(self):
         return 400
 
 
 class DownloadFileError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
-
     @property
     def error_code(self):
         return 598
 
 
 class PipelineIlegalError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
-
     @property
     def error_code(self):
         return 400
 
 
 class RPCExecError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
-
     @property
     def error_code(self):
         return 503
 
-class RequestError(Error):
-    def __init__(self, message, error):
-        self.message = message
-        self.error = error
 
+class RequestError(Error):
     @property
     def error_code(self):
         return 400
