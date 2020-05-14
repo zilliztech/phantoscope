152b7ca994fb88876ec117ce18fe872d3675aeae
れいげん あらたか <boyan.wang@zilliz.com>
2020-05-14 22:20:43 +0800

    delete unused file

--- search/common/util.py
+++ search/common/util.py
@@ -8,40 +8,6 @@ import base64
 from common.error import DecodeError, DownloadFileError
 from common.config import ALLOWED_IMAGE_EXTENSIONS
 
-def mkdir_p(path):
-    try:
-        os.makedirs(path)
-    except OSError as exc:  # Python >2.5
-        if exc.errno == errno.EEXIST and os.path.isdir(path):
-            pass
-        else:
-            raise
-
-
-def temp_directory():
-    path = os.path.expanduser('~/.tmp/resources')
-    mkdir_p(path)
-    return path
-
-
-def download_temp_file(url, local_path=None, untar=False):
-    if local_path is None:
-        local_path = url.rsplit('/', 1)[-1]
-    local_path = os.path.join(temp_directory(), local_path)
-    mkdir_p(os.path.dirname(local_path))
-    if not os.path.isfile(local_path):
-        print('Downloading {:s} to {:s}...'.format(url, local_path))
-        f = urllib.request.urlopen(url)
-        with open(local_path, 'wb') as local_f:
-            local_f.write(f.read())
-
-        if untar:
-            with tarfile.open(local_path) as tar_f:
-                tar_f.extractall(temp_directory())
-    if untar:
-        return temp_directory()
-    return local_path
-
 
 def save_tmp_file(name, file_data=None, url=None):
     DEFAULT_PATH = "./tmp/"
