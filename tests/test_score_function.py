5874fcb1558c051f60163ad6d5efc302e0e0e4e9
Abner <yuxing.yuan@zilliz.com>
2020-07-01 17:17:30 +0800

    add more test cases to coverage new api and features (#121)
    * edit application test
    
    * test application ok
    
    * test score function ok
    
    * add pull image before test
    
    * sleep for more time for docker create
    
    * add exception return message
    
    * add more wait time for docker run
    
    * fix exception name print
    
    * add more wait time during create instance
    
    * add more wait time during create instance
    
    * add more wait time during create instance
    
    * add more wait time during create instance
    
    * assign requests version
    
    * try start instances
    
    * delete application test for a try
    
    * add first wait
    
    * delete try
    
    * remove git pull from makefile
    
    * add try
    
    * add more timeout of docker client
    
    * reduce wait time
    
    * add application cover
    
    * add app & operator
    
    * add app & operator
    
    * add app & operator

--- tests/test_score_function.py
+++ tests/test_score_function.py
@@ -1,87 +1,103 @@
-import os
-import shutil
 import time
-import pytest
 from test_basic import client
-from test_basic import local_ip
-from utils.require import PreOperator, PrePipeline
+from utils.require import pre_instance
+from utils.require import pre_operator
+from utils.require import pre_pipeline
+from utils.require import pre_application
+from utils.require import sleep_time
 
 
 class TestScoreFunctionApi:
     """test class for application api"""
-    name = "pytestexample3"
-    field_name1 = 'detector'
-    field_name2 = 'none-detector'
-    detector_name = "pytest_ssd"
-    encoder_name = "pytest_vgg"
-    pipeline_name1 = "pytest_pipline3"
-    pipeline_name2 = "pytest_pipline4"
+    test_ver = 4
+    name = f"pytestexample{test_ver}"
+    field_name1 = f'detector{test_ver}'
+    field_name2 = f'none-detector{test_ver}'
+    encoder_addr = "psoperator/vgg16-encoder:latest"
+    encoder_type = "encoder"
+    encoder_name = f"pytestop{test_ver}"
+    encoder_instance = f"test_instance{test_ver}"
+    encoder_instance2 = f"test_instance2{test_ver}"
+
+    detector_addr = "psoperator/ssd-detector:latest"
+    detector_type = "processor"
+    detector_name = f"pytestop2{test_ver}"
+    detector_instance = f"test_instance3{test_ver}"
+
+    pipeline_name1 = f"pytest_pipline3{test_ver}"
+    pipeline_name2 = f"pytest_pipline4{test_ver}"
+
     inner_fields = ['first', 'random', 'avg', 'distance_first']
-    test_url = ['https://dss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=1906469856,4113625838&fm=26&gp=0.jpg',
-                'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592371648552&di=28d2be13f40539aacaa81ab5f43642e6&imgtype=0&src=http%3A%2F%2Fphotocdn.sohu.com%2F20151130%2Fmp45332712_1448864892504_4.jpeg',
-                'https://timgsa.baidu.com/timg?image&quality=80&size=b9999_10000&sec=1592375248588&di=13521ac5965776e504dde2e242610293&imgtype=0&src=http%3A%2F%2Ft8.baidu.com%2Fit%2Fu%3D2388511970%2C557698405%26fm%3D193',
-                'https://ss2.bdstatic.com/70cFvnSh_Q1YnxGkpoWK1HF6hhy/it/u=1668363641,4252982095&fm=26&gp=0.jpg']
+    score_modes = ['first', 'sum', 'max', 'min', 'multiple', 'avg']
+    test_url = ['https://live.staticflickr.com/65535/50064435471_0583cf876c_o.jpg',
+                'https://live.staticflickr.com/65535/50063878058_c7c04603cc_o.jpg',
+                'https://live.staticflickr.com/65535/50064692802_90aed5e68a_o.jpg',
+                'https://live.staticflickr.com/65535/50064709412_e204654f9b_o.jpg']
 
-    test_search_url = ['https://ss3.bdstatic.com/70cFv8Sh_Q1YnxGkpoWK1HF6hhy/it/u=3442599741,2386083112&fm=26&gp=0.jpg',
-                       'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1389311444,3919902992&fm=26&gp=0.jpg']
+    test_search_url = ['https://live.staticflickr.com/65535/50064411181_574e2e8eed_o.jpg',
+                       'https://live.staticflickr.com/65535/50064411221_6564587552_o.jpg']
 
-    def test_new_application_api(self, client):
-        PreOperator().create(
-            {"endpoint": f"{local_ip()}:50010", "name": self.detector_name})
-        PreOperator().create(
-            {"endpoint": f"{local_ip()}:50001", "name": self.encoder_name})
-        PrePipeline().create({
-            "name": self.pipeline_name1,
-            "input": "image",
-            "description": "this is a detector test pipeline",
-            "processors": self.detector_name,
-            "encoder": self.encoder_name,
-            "index_file_size": 1024
-        })
-        PrePipeline().create({
-            "name": self.pipeline_name2,
-            "input": "image",
-            "description": "this is a test pipeline",
-            "processors": "",
-            "encoder": self.encoder_name,
-            "index_file_size": 1024
-        })
-        data = {
-            'fields': {
-                self.field_name1: {
-                    'type': 'object',
-                    'pipeline': self.pipeline_name1
-                },
-                self.field_name2: {
-                    'type': 'object',
-                    'pipeline': self.pipeline_name2
-                }
-            },
-            's3Buckets': "s3example334"
-        }
-        rv = client.post(f'/v1/application/{self.name}', json=data)
-        json_data = rv.get_json()
-        assert rv.status_code == 200
-        assert json_data['_application_name'] == self.name
-
-    def test_upload_api(self, client):
+    @pre_operator(name=f"{encoder_name}2", type=encoder_type, addr=encoder_addr, version="0.1", description="")
+    @pre_instance(operator_name=f"{encoder_name}2", name=f"{encoder_instance}2")
+    @pre_instance(operator_name=f"{encoder_name}2", name=f"{encoder_instance2}2")
+    @pre_operator(name=f"{detector_name}2", type=detector_type, addr=detector_addr, version="0.1", description="")
+    @pre_instance(operator_name=f"{detector_name}2", name=f"{detector_instance}2")
+    @pre_pipeline(name=f"{pipeline_name1}2",
+                  encoder={"name": f"{encoder_name}2", "instance": f"{encoder_instance}2"})
+    @pre_pipeline(name=f"{pipeline_name2}2",
+                  processors=[{"name": f"{detector_name}2", "instance": f"{detector_instance}2"}],
+                  encoder={"name": f"{encoder_name}2", "instance": f"{encoder_instance2}2"})
+    @pre_application(name=f"{name}",
+                     fields={field_name1: {"type": "pipeline", "value": f"{pipeline_name1}2"},
+                             field_name2: {"type": "pipeline", "value": f"{pipeline_name2}2"}},
+                     s3_buckets=f"s3example{test_ver}")
+    @sleep_time(12)  # wait for operator instance initialization
+    def test_inner_field_score_api(self, client):
         for image_url in self.test_url:
             data = {
                 'fields': {
-                    self.field_name1: {
+                    self.field_name2: {
                         'url': image_url
                     }
                 }
             }
             rv = client.post(f"/v1/application/{self.name}/upload", json=data)
             assert rv.status_code == 200
-        time.sleep(1)
+        time.sleep(1)  # wait for milvus
 
-    def test_score_function_inner(self, client):
+        # search in empty milvus collection
+        data = {
+            'fields': {
+                self.field_name1: {
+                    'url': self.test_search_url[0],
+                    'inner_field_score_mode': "first"
+                }
+            },
+            'topk': 3,
+            'nprobe': 10
+        }
+        rv = client.post(f"/v1/application/{self.name}/search", json=data)
+        assert rv.status_code != 200
+
+        # search with nonexist field
+        data = {
+            'fields': {
+                "wrong_field": {
+                    'url': self.test_search_url[0],
+                    'inner_field_score_mode': "first"
+                }
+            },
+            'topk': 3,
+            'nprobe': 10
+        }
+        rv = client.post(f"/v1/application/{self.name}/search", json=data)
+        assert rv.status_code != 200
+
+        # test inner filed score mode search
         for inner_field_score_mode in self.inner_fields:
             data = {
                 'fields': {
-                    self.field_name1: {
+                    self.field_name2: {
                         'url': self.test_search_url[0],
                         'inner_field_score_mode': inner_field_score_mode
                     }
@@ -94,12 +110,12 @@ class TestScoreFunctionApi:
             json_data = rv.get_json()
             assert len(json_data) == 3
 
-    def test_score_function_insufficent_topk(self, client):
+        # test score function insufficent topk search
         topk = len(self.test_url) * 10
         for inner_field_score_mode in self.inner_fields:
             data = {
                 'fields': {
-                    self.field_name1: {
+                    self.field_name2: {
                         'url': self.test_search_url[0],
                         'inner_field_score_mode': inner_field_score_mode
                     }
@@ -113,7 +129,7 @@ class TestScoreFunctionApi:
             assert len(json_data) > 0
             assert len(json_data) < topk
 
-    def test_multi_field_score_function(self, client):
+        # test multi field score function
         topk = len(self.test_url) * 10
         for inner_field_score_mode in self.inner_fields:
             data = {
@@ -134,7 +150,7 @@ class TestScoreFunctionApi:
             }
             pass
 
-    def test_entities_api(self, client):
+        # get and delete all entities
         rv = client.get(f"/v1/application/{self.name}/entity?num=100")
         assert rv.status_code == 200
         json_data = rv.get_json()
@@ -145,10 +161,69 @@ class TestScoreFunctionApi:
             json_reply = reply.get_json()
             assert json_reply['_id'] == data['_id']
 
-    def test_delete_application_api(self, client):
-        PrePipeline().delete({'name': self.pipeline_name1})
-        PrePipeline().delete({'name': self.pipeline_name2})
-        PreOperator().delete({'name': self.detector_name})
-        PreOperator().delete({'name': self.encoder_name})
-        rv = client.delete(f"/v1/application/{self.name}")
+    @pre_operator(name=f"{encoder_name}3", type=encoder_type, addr=encoder_addr, version="0.1", description="")
+    @pre_instance(operator_name=f"{encoder_name}3", name=f"{encoder_instance}3")
+    @pre_instance(operator_name=f"{encoder_name}3", name=f"{encoder_instance2}3")
+    @pre_operator(name=f"{detector_name}3", type=detector_type, addr=detector_addr, version="0.1", description="")
+    @pre_instance(operator_name=f"{detector_name}3", name=f"{detector_instance}3")
+    @pre_pipeline(name=f"{pipeline_name1}3",
+                  encoder={"name": f"{encoder_name}3", "instance": f"{encoder_instance}3"})
+    @pre_pipeline(name=f"{pipeline_name2}3",
+                  processors=[{"name": f"{detector_name}3", "instance": f"{detector_instance}3"}],
+                  encoder={"name": f"{encoder_name}3", "instance": f"{encoder_instance2}3"})
+    @pre_application(name=f"{name}1",
+                     fields={field_name1: {"type": "pipeline", "value": f"{pipeline_name1}3"},
+                             field_name2: {"type": "pipeline", "value": f"{pipeline_name2}3"}},
+                     s3_buckets=f"s3example{test_ver}")
+    @sleep_time(12)  # wait for operator instance initialization
+    def test_score_api(self, client):
+        for image_url in self.test_url:
+            data = {
+                'fields': {
+                    self.field_name1: {
+                        'url': image_url
+                    },
+                    self.field_name2: {
+                        'url': image_url
+                    }
+                }
+            }
+            rv = client.post(f"/v1/application/{self.name}1/upload", json=data)
+            assert rv.status_code == 200
+        time.sleep(1)  # wait for milvus
+
+        # test multi field score function
+        topk = len(self.test_url) - 1
+        for score_mode in self.score_modes:
+            data = {
+                'fields': {
+                    self.field_name1: {
+                        'url': self.test_search_url[0],
+                        'inner_field_score_mode': 'first',
+                        'weight': 6
+                    },
+                    self.field_name2: {
+                        'url': self.test_search_url[1],
+                        'inner_field_score_mode': 'random',
+                        'weight': 2
+                    },
+                    "score_mode": score_mode
+                },
+                'topk': topk,
+                'nprobe': 10
+            }
+            rv = client.post(f"/v1/application/{self.name}1/search", json=data)
+            assert rv.status_code == 200
+            json_data = rv.get_json()
+            assert len(json_data) == topk
+
+        # get and delete all entities
+        rv = client.get(f"/v1/application/{self.name}1/entity?num=100")
         assert rv.status_code == 200
+        json_data = rv.get_json()
+        for data in json_data:
+            reply = client.delete(
+                f"/v1/application/{self.name}1/entity/{data['_id']}")
+            assert reply.status_code == 200
+            json_reply = reply.get_json()
+            assert json_reply['_id'] == data['_id']
